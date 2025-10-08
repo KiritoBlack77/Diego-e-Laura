dc8d8542462a99ca1c99b2ff0239cafe04b7671e
KiritoBlack77 <128146043+KiritoBlack77@users.noreply.github.com>
2025-10-08 15:37:44 +0000

    teste

--- public/index.php
+++ public/index.php
@@ -1,17 +1,129 @@
-<?php
-require '../vendor/autoload.php';
+<?php require_once("./includes/page-top.php") ?>
 
-use Application\Hello;
-?>
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <link href="/css/default.css" rel="stylesheet" />
-    <title>Document</title>
-</head>
-<body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
-</body>
-</html>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="./img/1.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/2.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/3.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/4.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/5.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/6.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/7.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/8.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/9.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/10.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/11.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/12.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/13.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/14.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/15.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/16.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/17.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+             <div class="carousel-item">
+                <img src="./img/18.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/19.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/20.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/21.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/22.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/23.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/24.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/25.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/26.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/27.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/28.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/29.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/30.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/31.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/32.jpg" class="d-block" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="./img/33.jpg" class="d-block" alt="..." style="width: 890px; height: auto;">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+
+    <main class="container p-3">
+        <div class="row">
+            <div class="col">
+                <div class="card text-white bg-warning">
+                    <div class="card-body">
+                        <p class="card-text">Laura meu amor por você cresce mais e mais a cada dia que passa. No momento que você mandou aquele Oi minha vida mudou completamente, porque apartir dali eu começei a me dedicar completamente a você, você é a minha companheira, minha amiga, minha alegria, minha vida, meu tudo. Laura você é a mulher que eu quero compartilhar todos os meus sucessos, quero compartilhar minha vida com você, quero poder construir uma familia ao seu lado. Por isso Laura Rodrigues da Rosa EU TE AMO.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <?php require_once("./includes/page-botton.php") ?>
